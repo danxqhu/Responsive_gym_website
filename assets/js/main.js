8a32fafc4527cf7cdf87deb5be81b0eb3504537b
xinqiang-hu <danxqhu@gmail.com>
2022-08-22 18:02:26 +0800

    media query

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,8 +38,33 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
@@ -86,3 +111,34 @@ const calculateBmi = e => {
 calculateForm.addEventListener('submit', calculateBmi);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+  contactMessage = document.getElementById('contact-message'),
+  contactUser = document.getElementById('contact-user');
+
+const sendEmail = e => {
+  e.preventDefault();
+  if (contactUser.value === '') {
+    contactMessage.classList.remove('color-green');
+    contactMessage.classList.add('color-red');
+    contactMessage.textContent = '👉 You must enter your email';
+    setTimeout(() => {
+      contactMessage.textContent = '';
+    }, 3000);
+  } else {
+    emailjs.sendForm('service_dk6aggj', 'template_1mrzmsb', '#contact-form', 'ZfrzbLG9t15lAxHvG').then(
+      () => {
+        contactMessage.classList.add('color-green');
+        contactMessage.textContent = 'You registered successfully';
+        setTimeout(() => {
+          contactMessage.textContent = '';
+        }, 3000);
+      },
+      error => {
+        alert('	OOPS! SOMETHING HAS FAILED...', error);
+      },
+    );
+    contactUser.value = '';
+  }
+};
+
+contactForm.addEventListener('submit', sendEmail);
