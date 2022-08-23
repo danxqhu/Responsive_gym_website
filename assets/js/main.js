682fa1e6c9b20c486a076834c9f04ed5bb72f2cf
xinqiang-hu <danxqhu@gmail.com>
2022-08-23 14:22:04 +0800

    finished project

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,6 +67,17 @@ const scrollUp = () => {
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
+sr.reveal(`.home__data, .footer__conainter, .footer__group`);
+sr.reveal(`.home__img`, { delay: 700, origin: 'bottom' });
+sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 });
+sr.reveal(`.choose__image, .calculate__content`, { origin: 'left' });
+sr.reveal(`.choose__content, .calculate__img`, { origin: 'right' });
 
 /*=============== CALCULATE JS ===============*/
 const calculateForm = document.getElementById('calculate-form'),
